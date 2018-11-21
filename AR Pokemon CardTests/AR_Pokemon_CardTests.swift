8acd8e8bf301d0c41494d3c8c6154e38cef68517
u6625166 <u6625166@anu.edu.au>
2018-11-21 14:03:23 +1100

    Initial Commit

---
+++ AR Pokemon CardTests/AR_Pokemon_CardTests.swift
@@ -0,0 +1,34 @@
+//
+//  AR_Pokemon_CardTests.swift
+//  AR Pokemon CardTests
+//
+//  Created by 杨偲 on 21/11/18.
+//  Copyright © 2018 杨偲. All rights reserved.
+//
+
+import XCTest
+@testable import AR_Pokemon_Card
+
+class AR_Pokemon_CardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
